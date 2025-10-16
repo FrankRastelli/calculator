da67d3c426dc08a130e3deecd91d470a44aa0fcf
Frank Rastelli <frankice8910@gmail.com>
2025-10-16 19:16:10 -0400

    Add operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let num1;
+let operator;
+let num2;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -16,5 +20,20 @@ function divide(num1, num2) {
     }
     else {
         return num1 / num2;
+    }
+}
+
+function operate(num1, num2, operator) {
+    if (operator === "+") {
+        return add(num1, num2);
+    }
+    else if (operator === "-") {
+        return subtract(num1, num2);
+    }
+    else if (operator === "*") {
+        return multiply(num1, num2) 
+    }
+    else if (operator === "/") {
+        return divide(num1, num2);
     }
 }
