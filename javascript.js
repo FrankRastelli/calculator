3d097248f3427a7dc9e8fe90a8c2abd0042fefae
Frank Rastelli <frankice8910@gmail.com>
2025-10-20 17:43:26 -0400

    Add evaluate function and operator button logic
    Basic operator buttons now update display with correct value.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
-let num1;
-let operator;
-let num2;
+let firstValue = null;
+let selectedOperator = null;
+let evaluated = null;
+let secondValue = null;
 
 let displayValue = "0";
 
@@ -41,7 +42,7 @@ function operate(a, b, operator) {
 }
 
 
-// display
+// -- display --
 const screen = document.querySelector(".screen-content");
 
 function updateScreen() {
@@ -71,4 +72,66 @@ document.querySelectorAll(".number").forEach(btn => {
     btn.addEventListener("click", () => {
         inputDigit(btn.textContent.trim());
     });
-})
+});
+
+// calculator logic
+// so user enters first number and then selects an operator. Once an operator is selected
+// we store the first number in a variable, and must indicate that it is time to get the
+// next number. So i need a variable that will say an operator has been selected, so lets
+// get ready for the second number. Once the second number is entered, the user can either,
+// select another operator to chain, or press equals (for now).
+
+const addition = document.querySelector(".add");
+const subtraction = document.querySelector(".subtraction");
+const multiplication = document.querySelector(".multiplication");
+const division = document.querySelector(".division");
+
+addition.addEventListener("click", () => {
+    evaluate("+");
+});
+
+subtraction.addEventListener("click", () => {
+    evaluate("-");
+});
+
+multiplication.addEventListener("click", () => {
+    evaluate("*");
+});
+
+division.addEventListener("click", () => {
+    evaluate("/");
+});
+
+
+function evaluate(operator) {
+    // if this is the first operator selected
+    if (selectedOperator === null) {
+        selectedOperator = operator;
+        
+        // get first value
+        firstValue = parseInt(displayValue);
+
+        // get display ready for second number
+        displayValue = "0"; 
+    }
+    else {
+        // get second value
+        secondValue = parseInt(displayValue);
+
+        // perform operation
+        evaluated = operate(firstValue, secondValue, selectedOperator);
+
+        firstValue = evaluated;
+
+        secondValue = null;
+
+        displayValue = firstValue;
+
+        updateScreen();
+
+        displayValue = "0";
+
+        selectedOperator = operator;
+    }
+    
+}
